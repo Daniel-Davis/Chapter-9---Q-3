a3100f60713d097eaba3903e8158cd273fa7c0c0
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-19 15:48:46 -0700

    Update NumbersAgain.js

--- NumbersAgain.js
+++ NumbersAgain.js
@@ -1,4 +1,4 @@
 // Daniel Davis, NumbersAgain, must write an expression that matches only Javascript-style numbers //
 
-var expression = /^\d+/||\d+/[eE]\d+/||[+\-]\d+/||[+\-]\d+/[eE]\d+/||\d+/[+\-]||\d+/[eE]\d+/[+\-]$/;
+var expression = /^\d+||\d+[eE]\d+||[+\-]\d+||[+\-]\d+[eE]\d+||\d+[+\-]||\d+[eE]\d+[+\-]$/;
 
